8f7a88223ec2e9016287787a5f6176b5dc6a7ee3
Arsh Alam <arshalam724@gmail.com>
2026-01-20 11:51:48 +0530

    Initialise the project

---
+++ src/test/java/com/example/day3lms/Day3LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day3lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3LmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
